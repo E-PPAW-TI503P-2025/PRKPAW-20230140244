31adecf50da2303af2fedab8781eb448df227dfc
Aswun <aswinlutfian32@gmail.com>
2025-11-02 20:35:51 +0700

    menambahkah fitur agar dapat filter berdasarkan rentang waktu tanggal

--- 20230140244-node-server/controllers/reportController.js
+++ 20230140244-node-server/controllers/reportController.js
@@ -1,11 +1,10 @@
 const presensiRecords = require("../data/presensiData");
-
 const { Presensi } = require("../models");
 const { Op } = require("sequelize");
 
 exports.getDailyReport = async (req, res) => {
   try {
-    const { nama } = req.query;
+    const { nama, tanggalMulai, tanggalSelesai } = req.query;
     let options = { where: {} };
 
     if (nama) {
@@ -14,15 +13,35 @@ exports.getDailyReport = async (req, res) => {
       };
     }
 
+    if (tanggalMulai && tanggalSelesai) {
+      options.where.tanggal = {
+        [Op.between]: [tanggalMulai, tanggalSelesai],
+      };
+    } else if (tanggalMulai) {
+      options.where.tanggal = {
+        [Op.gte]: tanggalMulai,
+      };
+    } else if (tanggalSelesai) {
+      options.where.tanggal = {
+        [Op.lte]: tanggalSelesai,
+      };
+    }
+
     const records = await Presensi.findAll(options);
 
     res.json({
       reportDate: new Date().toLocaleDateString(),
+      filter: {
+        nama: nama || null,
+        tanggalMulai: tanggalMulai || null,
+        tanggalSelesai: tanggalSelesai || null,
+      },
       data: records,
     });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Gagal mengambil laporan", error: error.message });
+    res.status(500).json({
+      message: "Gagal mengambil laporan",
+      error: error.message,
+    });
   }
 };
